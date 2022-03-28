0417bb3f3cbb38e2ac67f4428f5c8149a9d088de
Jeong_suho <70904115+wjdtngh950@users.noreply.github.com>
2022-03-28 21:46:25 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/language_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/language_kotlin/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.language_kotlin.databinding.ActivityMainBinding
-dfdfdsafsadf
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
