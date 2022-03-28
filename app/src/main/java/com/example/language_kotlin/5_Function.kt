8b0b473b12e23878a593e2072b32d711aa7d77e0
Jeong_suho <70904115+wjdtngh950@users.noreply.github.com>
2022-03-28 23:19:26 +0900

    Update 5_Function.kt

--- app/src/main/java/com/example/language_kotlin/5_Function.kt
+++ app/src/main/java/com/example/language_kotlin/5_Function.kt
@@ -1,2 +1,14 @@
 package com.example.language_kotlin
 
+fun main(){
+    println(add(5, 6, 7)) // 밑에 add 함수를 보면 a=Int형의 5, b=Int형의 6, c=Int형의 7과 대치됨!
+}
+
+fun add(a: Int, b: Int, c: Int): Int{
+  return a+b+c // return이 나오면 함수를 종료하고 결과값을 나타냄 여기선 함수가 종료되고 a+b+c를 반환하게됨!
+}
+//위 처럼 간단하게 만들 수 있는 함수는 이렇게도 가능함! 단일 표현식 함수라고 표현
+//fun add(a: Int, b: Int, c: Int) = a + b + c
+
+
+
